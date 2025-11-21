553047a6591762e04d1a3b86d67c17d646274312
rendrakb <rendrakb@gmail.com>
2025-11-22 01:45:40 +0700

    feedback tweak

--- main.js
+++ main.js
@@ -246,7 +246,7 @@ document.getElementById("submitAnswerButton").addEventListener("click", () => {
     feedback.textContent = "Correct.";
     feedback.style.color = "lightgreen";
   } else {
-    feedback.textContent = `Wrong, it's: ${currentAnswer}`;
+    feedback.textContent = "Wrong";
     feedback.style.color = "red";
   }
 });
